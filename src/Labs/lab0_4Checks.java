c29499e7fa2398b26edea6089e645c2ef893cf3d
Michael <mcardenas389@users.noreply.github.com>
2017-01-05 14:35:16 -0500

    Add files via upload

--- src/Labs/lab0_4Checks.java
+++ src/Labs/lab0_4Checks.java
@@ -1,31 +1,50 @@
 package Labs;
 
-//import blackboard.platform.context.Context;
-
 public class lab0_4Checks extends inputChecks
 {
-    public lab0_4Checks(int x, int y)
+    public lab0_4Checks(String tableName, int x, int y, 
+			String userid, String courseid, int labNumber)
     {
-        super(x,y,"lab0_4");
+    	super(tableName, x, y, userid, courseid, labNumber);
     }
     
     @Override
     protected void buildKey()
     {
-        setKey(0,0,"clear");
-        setKey(0,1,"light");
-        setKey(0,2,"ionic");
-        
-        setKey(1,0,"clear");
-        setKey(1,1,"no light");
-        setKey(1,2,"covalent");
-        
-        setKey(2,0,"blue");
-        setKey(2,1,"light");
-        setKey(2,2,"ionic");
-        
-        setKey(3,0,"blue");
-        setKey(3,1,"dim light");
-        setKey(3,2,"ionic");
+        key = new String[][] {
+        	{"clear", "light", "ionic"},
+        	{"clear", "no light", "covalent"},
+        	{"blue", "light", "ionic"},
+        	{"blue", "dim light", "ionic"}
+        };
+    }
+    
+    public void gradeLab(int labNumber, String userid, String courseid)
+    {
+    	double gradeTotal = 0.0; //stores total grade
+		double score = 1.0; //score for correct answer
+		
+    	buildKey();
+    	
+    	for(int i = 0; i < dataX; i++)
+    	{
+    		for(int j = 0; j < dataY; j++)
+    		{
+    			if(key[i][j].equalsIgnoreCase(data[i][j]))
+    			{
+    				isCorrect[i][j] = correctMsg;
+					grade[i][j] = "" + score;
+					gradeTotal += score;
+    			}
+    			else
+    			{
+    				isCorrect[i][j] = errorMsg;
+					grade[i][j] = "-" + score; //deducted points
+					error[i][j] = errorTypes[3]; //generic error  
+    			}
+    		}
+    	}
+    	
+    	//submit(labNumber, userid, courseid, gradeTotal);
     }
 }
